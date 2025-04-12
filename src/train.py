2c9209b2cdedf75a0b14edeba262acb58a053270
Jamal Mapp <129474735+Jamal-Mapp@users.noreply.github.com>
2025-04-11 20:20:40 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,11 @@
+import torch
+from model import Generator, Discriminator
+
+def train():
+    gen = Generator().cuda()
+    disc = Discriminator().cuda()
+    print("✅ Models initialized")
+
+if __name__ == "__main__":
+    train()
 
